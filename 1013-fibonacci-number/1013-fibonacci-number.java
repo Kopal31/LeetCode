9337e3eb566bc63a7574a3c75a760799d8ae4349
Kopal Soni <91530366+Kopal31@users.noreply.github.com>
2024-11-28 11:49:18 +0530

    Time: 9 ms (24.49%), Space: 39.9 MB (95.74%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+        // Base cases
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        // Recursive call
+        return fib(n - 1) + fib(n - 2);
+    }
+}
