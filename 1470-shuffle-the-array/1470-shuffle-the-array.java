de5fedc49b9169c52a42a327cab17ed6e02dbfb8
Kopal Soni <91530366+Kopal31@users.noreply.github.com>
2026-01-01 19:33:08 +0530

    Time: 0 ms (100%), Space: 46.6 MB (19.1%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans[idx++] = nums[i];     // xi
+            ans[idx++] = nums[i + n]; // yi
+        }
+        return ans;
+    }
+}
