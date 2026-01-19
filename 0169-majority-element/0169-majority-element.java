e9e389ad2c6a71258b3e104d569ee48fd2dc18fa
Kopal Soni <91530366+Kopal31@users.noreply.github.com>
2026-01-19 22:09:01 +0530

    Time: 2 ms (72.3%), Space: 55.7 MB (52.26%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,34 @@
+//import java.util.HashMap;
+//import java.util.Map;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int cnt = 0;
+        int el = 0;
+
+        for(int i = 0; i < n; i++){
+            if(cnt == 0){
+                cnt = 1;
+                el = nums[i];
+            }
+            else if(el == nums[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+
+        int cnt1 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == el)
+                cnt1++;
+        }
+
+        if(cnt1 > (n/2))
+            return el;
+        
+        return -1; // This line should never be reached if there's always a majority element
+    }
+}
