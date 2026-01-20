d69d8eabe614e2c211b5287db1059fca190808f5
Kopal Soni <91530366+Kopal31@users.noreply.github.com>
2026-01-21 03:49:50 +0530

    Time: 4 ms (94.07%), Space: 44.6 MB (44.58%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,4 +1,4 @@
-
+/*
 class Solution {
     public String reverseWords(String s) {
         
@@ -16,3 +16,31 @@ class Solution {
         return ans.toString();
     }
 }
+*/
+
+class Solution {
+    public String reverseWords(String s) {
+        int n = s.length();
+        StringBuilder ans = new StringBuilder();
+
+        int i = n-1;
+
+        while(i >= 0){
+            //skip the trailing spaces
+
+            while(i >=0 && s.charAt(i) == ' ')i--;
+
+            if(i<0) break;
+
+            int j = i;
+
+            while(i >= 0 && s.charAt(i) != ' ')i--;
+            ans.append(s.substring(i+1, j+1));
+
+            ans.append(" ");
+        }
+
+        return ans.toString().trim();
+    }
+
+    }
