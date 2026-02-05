caf2620a0ed197de9c939ff06bf4f4f4dc722220
Kopal Soni <91530366+Kopal31@users.noreply.github.com>
2026-02-06 01:57:20 +0530

    Time: 1 ms (98.74%), Space: 46.6 MB (86.48%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                int newIndex = (i + nums[i]) % n;
+
+                if (newIndex < 0) {
+                    newIndex += n;
+                }
+
+                result[i] = nums[newIndex];
+            }
+        }
+
+        return result;
+    }
+}
